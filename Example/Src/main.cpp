769705a7934f719d70c6279e4063cd3956d76677
Thomas Breuer <thomas.breuer@h-brs.de>
2024-11-08 12:10:06 +0100

    List of examples (main.cpp) refined

--- Example/Src/main.cpp
+++ Example/Src/main.cpp
@@ -17,89 +17,102 @@ Attention:
 // Uncomment the desired example file.
 //*******************************************************************
 
+
+/**
+  Device
+  ------
+    V   Virtual
+    S1  STM32F407-Discovery
+    S2  STM32F746-Discovery
+    S3  STM32F769-Discovery
+    S4  STM32L100-Discovery
+    S5  STM32-Nucleo32-L432
+    A1  ATmega32u4-Leonardo
+
+
+  Test mark
+  ---------
+    x tested
+    * not tested
+    - not implemented
+    ? todo
+
+**/
 //===================================================================
 // Demo
 //===================================================================
-                                                                      // Virtual  STM32  ATmega
+                                                                      //  V  S1 S2 S3 S4 S5 A1
+                                                                      //  --------------------
 ///
-#include "Main/Demo/Blinky/main.cpp"                               //  X        X      X
-///#include "Main/Demo/LED/main.cpp"                                  //  X        X      X
+#include "Main/Demo/Blinky/main.cpp"                               //  x  x  x  x  x  x  x
+///#include "Main/Demo/LED/main.cpp"                                  //  x  x  x  x  x  x  x
 
 //===================================================================
 // Device
 //===================================================================
-                                                                      // Virtual  STM32  ATmega
-///#include "Main/Device/DevAnalog.cpp"                               //  X        X     (1)
-///#include "Main/Device/DevDigital.cpp"                              //  X        X      X
-///#include "Main/Device/DevPointer.cpp"                              //  X        -      -
-///#include "Main/Device/DevTaskManager.cpp"                          //  X        X      X
-///#include "Main/Device/DevTerminal.cpp"                             //  X        X      X
-///#include "Main/Device/DevScreenChar.cpp"                           //  X        X      -
-///#include "Main/Device/DevScreenGraphic.cpp"                        //  X        -      -
-/*
-  (1) Todo: add implementation in config.h
-*/
+                                                                      //  V  S1 S2 S3 S4 S5 A1
+                                                                      //  --------------------
+///#include "Main/Device/DevAnalog.cpp"                               //  x  -  -  -  -  x  -
+///#include "Main/Device/DevDigital.cpp"                              //  x  -  -  -  -  x  -
+///#include "Main/Device/DevPointer.cpp"                              //  x  x  x  x  -  -  -
+///#include "Main/Device/DevTaskManager.cpp"                          //  x  x  x  x  x  x  x
+///#include "Main/Device/DevTerminal.cpp"                             //  x  x  x  x  x  x  x
+///#include "Main/Device/DevScreenChar.cpp"                           //  x  x  x  x  x  x  -
+///#include "Main/Device/DevScreenGraphic.cpp"                        //  x  x  x  x  -  -  -
 
 //===================================================================
 // Control
 //===================================================================
-                                                                      // Virtual  STM32  ATmega
-///#include "Main/Control/CtrlDigitalIndicator.cpp"                   //  X        X      X
-///#include "Main/Control/CtrlDigitalButton.cpp"                      //  X        X      X
-///#include "Main/Control/CtrlDigitalEncoder.cpp"                     //  X        X(1)   -
-
-/*
-  (1) STM32F407-Discovery only
-*/
+                                                                      //  V  S1 S2 S3 S4 S5 A1
+                                                                      //  --------------------
+///#include "Main/Control/CtrlDigitalIndicator.cpp"                   //  x  x  x  x  x  -  x
+///#include "Main/Control/CtrlDigitalButton.cpp"                      //  x  x  x  x  x  -  x
+///#include "Main/Control/CtrlDigitalEncoder.cpp"                     //  x  x  *  x  *  *  -
 
 //===================================================================
 // Hardware
 //===================================================================
-                                                                      // Virtual  STM32  ATmega
-///#include "Main/Hardware/HwAdc.cpp"                                 //  X        X      X
-///#include "Main/Hardware/HwDac.cpp"                                 //  X        X      -
-///#include "Main/Hardware/HwDisplayChar.cpp"                         //  X        X(1)   -
-///#include "Main/Hardware/HwDisplayGraphic.cpp"                      //  X        X(1)   -
-///#include "Main/Hardware/HwEncoder.cpp"                             //  -        X      -
-///#include "Main/Hardware/HwExtInt.cpp"                              //  -        X(3)   -
-///#include "Main/Hardware/HwI2Cmaster.cpp"                           //  -        X      X(2)
-///#include "Main/Hardware/HwI2Cslave.cpp"                            //  -        -      X
-///#include "Main/Hardware/HwMemory.cpp"                              //  X        X      X
-///#include "Main/Hardware/HwNet_TCP.cpp"                             //  X        X      -
-///#include "Main/Hardware/HwNet_UDP.cpp"                             //  X        X      -
-///#include "Main/Hardware/HwPort.cpp"                                //  X        X      X
-///#include "Main/Hardware/HwPortPin.cpp"                             //  X        X      X
-///#include "Main/Hardware/HwRtc.cpp"                                 //  X        X      -
-///#include "Main/Hardware/HwRtos.cpp"                                //  X        X      X
-///#include "Main/Hardware/HwSPImaster.cpp"                           //  -        X      X(2)
-///#include "Main/Hardware/HwSPIslave.cpp"                            //  -        -      X
-///#include "Main/Hardware/HwTimer.cpp"                               //  X        X      X
-///#include "Main/Hardware/HwTouch.cpp"                               //  X        -      -
-///#include "Main/Hardware/HwUart.cpp"                                //  X        X      X
-///#include "Main/Hardware/HwUSBdevice.cpp"                           //  -        X      X
-///#include "Main/Hardware/HwUSBhost.cpp"                             //  X        -      -
+                                                                      //  V  S1 S2 S3 S4 S5 A1
+                                                                      //  --------------------
+///#include "Main/Hardware/HwAdc.cpp"                                 //  x  x  x  x  x  x  x
+///#include "Main/Hardware/HwDac.cpp"                                 //  x  x  -  x  x  x  -
+///#include "Main/Hardware/HwDisplayChar.cpp"                         //  x  x  x  x  x  *  -
+///#include "Main/Hardware/HwDisplayGraphic.cpp"                      //  x  x  x  x  -  *  -
+///#include "Main/Hardware/HwEncoder.cpp"                             //  -  x  -  *  x  ?  -
+///#include "Main/Hardware/HwExtInt.cpp"                              //  -  x  *  *  x  ?  -
+///#include "Main/Hardware/HwI2Cmaster.cpp"                           //  -  x  *  *  x  x  x
+///#include "Main/Hardware/HwI2Cslave.cpp"                            //  -  -  -  -  -  -  x
+///#include "Main/Hardware/HwMemory.cpp"                              //  x  x  x  x  x  x  x
+///#include "Main/Hardware/HwNet_TCP.cpp"                             //  x  x  x  x  *  *  -
+///#include "Main/Hardware/HwNet_UDP.cpp"                             //  x  x  x  x  *  *  -
+///#include "Main/Hardware/HwPort.cpp"                                //  x  x  x  x  x  x  x
+///#include "Main/Hardware/HwPortPin.cpp"                             //  x  x  x  x  x  x  x
+///#include "Main/Hardware/HwRtc.cpp"                                 //  x  x  x  x  x  x  -
+///#include "Main/Hardware/HwRtos.cpp"                                //  x  x  x  x  x  x  x
+///#include "Main/Hardware/HwSPImaster.cpp"                           //  -  x  ?  x  x  ?  ?
+///#include "Main/Hardware/HwSPIslave.cpp"                            //  -  -  -  -  -  -  x
+///#include "Main/Hardware/HwTimer.cpp"                               //  1  x  x  x  x  x  x
+///#include "Main/Hardware/HwTouch.cpp"                               //  x  x  x  x  -  *  -
+///#include "Main/Hardware/HwUart.cpp"                                //  x  x  x  x  x  x  x
+///#include "Main/Hardware/HwUSBdevice.cpp"                           //  -  x  x  x  x  x  x
+///#include "Main/Hardware/HwUSBhost.cpp"                             //  x  -  -  -  -  -  -
 
 /*
-  (1) STM32F407-Discovery only
-  (2) not tested
-  (3) STM32-Nucleo32-L432 and STM32F4-Discovery
+  (1) ohne PWM
 */
 
 //===================================================================
 // Hardware/Peripheral
 //===================================================================
-                                                                      // Virtual  STM32  ATmega
-///#include "Main/Hardware/Peripheral/HwDisplayChar_DIP204spi.cpp"    //  X        X(1)   X
-///#include "Main/Hardware/Peripheral/HwDisplayChar_Terminal.cpp"     //  X        X      X
-///#include "Main/Hardware/Peripheral/HwDisplayGraphic_SSD1306i2c.cpp" //           X(2)
-///#include "Main/Hardware/Peripheral/HwMemory_PCF8583.cpp"           //  -        X      -
-///#include "Main/Hardware/Peripheral/HwPort_PCF8574.cpp"             //  -        X      X
-///#include "Main/Hardware/Peripheral/HwRtc_PCF8583.cpp"              //  -        X      -
-
-/*
-  (1) STM32F407-Discovery only
-  (2) STM32-Nucleo32-L432 only
-*/
+                                                                      //  V  S1 S2 S3 S4 S5 A1
+                                                                      //  --------------------
+///#include "Main/Hardware/Peripheral/HwDisplayChar_DIP204spi.cpp"    //  -  *  *  *  x  *  x
+///#include "Main/Hardware/Peripheral/HwDisplayChar_Terminal.cpp"     //  -  x  x  x  x  *  x
+///#include "Main/Hardware/Peripheral/HwDisplayGraphic_SSD1306i2c.cpp"//  -  *  *  *  *  x  -
+///#include "Main/Hardware/Peripheral/HwMemory_PCF8583.cpp"           //  -  *  *  *  *  *  -
+///#include "Main/Hardware/Peripheral/HwPort_PCF8574.cpp"             //  -  *  *  *  *  *  x
+///#include "Main/Hardware/Peripheral/HwRtc_PCF8583.cpp"              //  -  *  *  *  *  *  -
+///#include "Main/Hardware/Peripheral/HwEthernet_ENC28J60.cpp"        //  -  *  *  *  *  *  -
 
 //===================================================================
 // Module
@@ -107,41 +120,58 @@ Attention:
 //-------------------------------------------------------------------
 // Module/Isc
 //-------------------------------------------------------------------
-                                                                      // Virtual  STM32  ATmega
-///#include "Main/Module/Isc/ModIsc.cpp"                             //  X        X      X
+                                                                      //  V  S1 S2 S3 S4 S5 A1
+                                                                      //  --------------------
+///#include "Main/Module/Isc/ModIsc.cpp"                              //  x  *  *  ?  ?  ?  x
 
 //-------------------------------------------------------------------
 // Module/Rtos
 //-------------------------------------------------------------------
-                                                                      // Virtual  STM32  ATmega
-///#include "Main/Module/Rtos/ModRtos.cpp"                            //  X        X      X
+                                                                      //  V  S1 S2 S3 S4 S5 A1
+                                                                      //  --------------------
+///#include "Main/Module/Rtos/ModRtos.cpp"                            //  x  x  x  x  x  x  x
 
 //-------------------------------------------------------------------
 // Module/USB
 //-------------------------------------------------------------------
-                                                                      // Virtual  STM32  ATmega
-///#include "Main/Module/USB/ModUSB_Uart.cpp"                         // -         X(1)   X
-///#include "Main/Module/USB/ModUSBdeviceSimpleIO.cpp"                // -         X      X
-///#include "Main/Module/USB/ModUSBkeyboard.cpp"                      // -         X      X
-///#include "Main/Module/USB/ModUSBmouse.cpp"                         // -         X      X
-///#include "Main/Module/USB/ModUSBvolume.cpp"                        // -         X      X
+                                                                      //  V  S1 S2 S3 S4 S5 A1
+                                                                      //  --------------------
+///#include "Main/Module/USB/ModUSB_Uart.cpp"                         //  -  1  1  1  1  1  x
+///#include "Main/Module/USB/ModUSBdeviceSimpleIO.cpp"                //  -  x  *  x  x  x  x
+///#include "Main/Module/USB/ModUSBkeyboard.cpp"                      //  -  x  x  x  x  x  x
+///#include "Main/Module/USB/ModUSBmouse.cpp"                         //  -  x  x  x  x  x  x
+///#include "Main/Module/USB/ModUSBvolume.cpp"                        //  -  x  x  x  x  x  x
+
+/**
+Sx   Not ok with uVision
+(1)  Not ok, bulk transfer not running
+**/
+
+//-------------------------------------------------------------------
+// Module/Mqtt
+//-------------------------------------------------------------------
+                                                                      //  V  S1 S2 S3 S4 S5 A1
+                                                                      //  --------------------
+///#include "Main/Module/Mqtt/ModMqtt.cpp"                            //  x  *  *  x  *  *  -
+///#include "Main/Module/Mqtt/ModMqttClient.cpp"                      //  x  *  *  x  *  *  -
 
 /*
-  (1) STM32F100-Discovery only
+  (1) STM32F769-Discovery only
 */
 
 //===================================================================
 // Std
 //===================================================================
-                                                                      // Virtual  STM32  ATmega
-///#include "Main/Std/StdClock.cpp"                                   //  X        X      X
-///#include "Main/Std/StdCrc.cpp"                                     //  X        X      X
-///#include "Main/Std/StdDataPointer.cpp"                             //  X        X      X
-///#include "Main/Std/StdFifo.cpp"                                    //  X        X      X
-///#include "Main/Std/StdFlag.cpp"                                    //  X        X      X
-///#include "Main/Std/StdReport.cpp"                                  //  X        X      X
-///#include "Main/Std/StdSequence.cpp"                                //  X        X      X
-///#include "Main/Std/StdSharedMem.cpp"                               //  X        X      X
+                                                                      //  V  S1 S2 S3 S4 S5 A1
+                                                                      //  --------------------
+///#include "Main/Std/StdClock.cpp"                                   //  x  *  x  *  x  *  x
+///#include "Main/Std/StdCrc.cpp"                                     //  x  *  x  *  x  *  x
+///#include "Main/Std/StdDataPointer.cpp"                             //  x  x  *  x  x  x  x
+///#include "Main/Std/StdFifo.cpp"                                    //  x  x  *  x  x  x  x
+///#include "Main/Std/StdFlag.cpp"                                    //  x  x  *  x  x  x  x
+///#include "Main/Std/StdReport.cpp"                                  //  x  x  *  x  x  x  x
+///#include "Main/Std/StdSequence.cpp"                                //  x  x  *  x  x  x  x
+///#include "Main/Std/StdSharedMem.cpp"                               //  x  *  x  *  x  *  x
 
 //##################################################################
 
